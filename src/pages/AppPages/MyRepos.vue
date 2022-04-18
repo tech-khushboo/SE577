c1435b9c47475c911da59023304dab65a1af557d
Khushboo Kumari <khushboo2502@gmail.com>
2022-04-19 03:33:09 +0530

    first commit

---
+++ src/pages/AppPages/MyRepos.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="q-pa-md row flex flex-center">
+    <q-card flat class="q-ma-md my-card bg-primary text-white">
+      <q-card-section>
+        <div class="text-h6">Repo 1</div>
+      </q-card-section>
+    </q-card>
+    <q-card flat class="q-ma-md my-card bg-secondary text-white">
+      <q-card-section>
+        <div class="text-h6">Repo 2</div>
+      </q-card-section>
+    </q-card>
+    <q-card flat class=" q-ma-md my-card bg-accent text-white">
+      <q-card-section>
+        <div class="text-h6">Repo 3</div>
+      </q-card-section>
+    </q-card>
+    <q-card flat class="q-ma-md my-card bg-warning text-white">
+      <q-card-section>
+        <div class="text-h6">Repo 4</div>
+      </q-card-section>
+    </q-card>
+  </div>
+  <!-- About, Background, Course, Publications, Photo-->
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MyRepoPage'
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
